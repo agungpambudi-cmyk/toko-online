b66d2817d6273e81adcbea9d3d0e30ca341cb411
agungpambudi-cmyk <hita56164@gmail.com>
2020-09-03 07:58:32 +0700

    peluang kerja kita

---
+++ menu.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="Dashboard">
+  <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
+  <title>Dashio - Bootstrap Admin Template</title>
+
+  <!-- Favicons -->
+  <link href="img/favicon.png" rel="icon">
+  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Bootstrap core CSS -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!--external css-->
+  <link href="lib/font-awesome/css/font-awesome.css" rel="stylesheet" />
+  <link rel="stylesheet" type="text/css" href="css/zabuto_calendar.css">
+  <link rel="stylesheet" type="text/css" href="lib/gritter/css/jquery.gritter.css" />
+  <!-- Custom styles for this template -->
+  <link href="css/style.css" rel="stylesheet">
+  <link href="css/style-responsive.css" rel="stylesheet">
+  <script src="lib/chart-master/Chart.js"></script>
+
+  <!-- =======================================================
+    Template Name: Dashio
+    Template URL: https://templatemag.com/dashio-bootstrap-admin-template/
+    Author: TemplateMag.com
+    License: https://templatemag.com/license/
+  ======================================================= -->
+</head>
+
+<body>
+          <li>
+            <a href="?p=carabeli">
+              <i class="fa fa-cogs"></i>
+              <span>cara pembelian </span>
+              </a>
+          </li>
+          <li>
+            <a href="?p=gantipass">
+              <i class="fa fa-cogs"></i>
+              <span>ganti password</span>
+              </a>
+          </li>
+
+  <li class="sub-menu">
+            <a href="javascript:;">
+              <i class="fa fa-th"></i>
+              <span>Master Produk</span>
+              </a>
+            <ul class="sub">
+              <li><a href="?p=produk">produk</a></li>
+              <li><a href="?p=kategori">kategori produk</a></li>
+              <li><a href="?p=sub-produk">sub produk</a></li>
+              <li><a href="?p=jasa">jasa pengirim</a></li>
+              <li><a href="?p=ongkir">ongkos kirim</a></li>
+            </ul>
+          </li>
+                 <li class="sub-menu">
+            <a href="javascript:;">
+              <i class="fa fa-book"></i>
+              <span>modul admin</span>
+              </a>
+            <ul class="sub">
+              <li><a href="?p=customer">Costumer Online</a></li>
+              <li><a href="?p=Rekening">Rekening bank</a></li>
+              <li><a href="?p=link">link terkait</a></li>
+            </ul>
+          </li>
+          <li class="sub-menu">
+            <a href="javascript:;">
+              <i class="fa fa-envelope"></i>
+              <span>pesan</span>
+              </a>
+            <ul class="sub">
+              <li><a href="?p=customer">komentar/a></li>
+              <li><a href="?p=Rekening">pesan</a></li>
+            </ul>
+          </li>
+            <li class="sub-menu">
+            <a href="javascript:;">
+              <i class="fa fa-book"></i>
+              <span>Menu transaksi</span>
+              </a>
+            <ul class="sub">
+              <li><a href="?p=order">Order Masuk</a></li>
+              <li><a href="?p=laporan">Laporan</a></li>
+            </ul>
+          </li>
+
+       
+          <li class="sub-menu">
+            <a href="javascript:;">
+              <i class="fa fa-comments-o"></i>
+              <span>Chat Room</span>
+              </a>
+            <ul class="sub">
+              <li><a href="lobby.html">Lobby</a></li>
+              <li><a href="chat_room.html"> Chat Room</a></li>
+            </ul>
+          </li>
+       
+</body>
+</html>
